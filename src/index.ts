0ded090d48c0752166386f75baed7cc8197e2bd7
Veronica Roures <veronica.roures@xarxatecactiva.com>
2024-10-27 09:49:34 +0100

    añado h1

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Express from 'express';
 const app = Express();
 const port = 3000;
 app.get('/', (req: Express.Request, res: Express.Response) => {
-res.send('Hello World!');
+res.send('<h1>Hello World!</h1>');
 });
 app.listen(port, () => {
 console.log(`App listening at port ${port}`);
