358096746cdaed3c45896d78ccf88f2079597a0b
Veronica Roures <veronica.roures@xarxatecactiva.com>
2024-10-27 09:27:13 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Express from 'express';
+const app = Express();
+const port = 3000;
+app.get('/', (req: Express.Request, res: Express.Response) => {
+res.send('Hello World!');
+});
+app.listen(port, () => {
+console.log(`App listening at port ${port}`);
+});
